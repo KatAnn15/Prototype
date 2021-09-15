95cae19c5bbb322b548b445883af4a79ddbc4225
= <katherineviegh@gmail.com>
2021-09-15 18:32:04 +0300

    login butto fixed

--- src/scripts/tsx/global_tsx/header_tsx/members-bar.tsx
+++ src/scripts/tsx/global_tsx/header_tsx/members-bar.tsx
@@ -34,7 +34,7 @@ const MembersBar :React.FC<MembersBarProps> = ({setModalVisibility}) => {
     return (
         <div className="members-bar_wrapper">
             {context.loggedIn ? <button className="member_welcome-message" onClick={toggleLoginBtnForMobile}>Welcome, {context.email?.split("@")[0]}</button>: null}
-            <button className="members-bar_login-button" style={{display: isMobile && !context.loggedIn? "block" : mLoginDisplay, top: isMobile &&context.loggedIn ? "75px" : "25px"}} onClick={setUserStatus}>{context.loggedIn ? "Log Out" : "Log In"}</button>
+            <button className="members-bar_login-button" style={{display: (isMobile && !context.loggedIn) || !isMobile ? "block" : mLoginDisplay, top: isMobile &&context.loggedIn ? "75px" : "25px"}} onClick={setUserStatus}>{context.loggedIn ? "Log Out" : "Log In"}</button>
         </div>
     )
 }
